7cb315efb7233ef30e58d707bf4dfea12459aa90
Demaceo Howard <hdemaceo@gmail.com>
2020-10-08 16:20:43 -0600

    Add constructor method to Round class

--- src/Round.js
+++ src/Round.js
@@ -1,5 +1,13 @@
-class Round {
+const Turn = require('../src/Turn');
 
-}
+class Round {
+  constructor(deck) {
+    this.deck = deck;
+    this.card = this.deck.cards[0];
+    this.turn = "";
+    this.turns = 0;
+    this.incorrectGuesses = [];
+  };
+};
 
 module.exports = Round;
