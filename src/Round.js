d54b005336f6e6b272e081b1c2ff7ea2de028973
Demaceo Howard <hdemaceo@gmail.com>
2020-10-08 16:21:59 -0600

    Add and create functionality for takeTurn method

--- src/Round.js
+++ src/Round.js
@@ -8,10 +8,21 @@ class Round {
     this.turns = 0;
     this.incorrectGuesses = [];
   };
+
   returnCurrentCard() {
     return this.card;
   };
-
+  
+  takeTurn(guess) {
+    this.turn = new Turn(guess, this.card);
+    this.deck.cards.shift();
+    this.card = this.deck.cards[0];
+    if(this.turn.evaluateGuess() === false) {
+      this.incorrectGuesses.unshift(this.turn.card.id);
+    };
+    this.turns += 1;
+    return this.turn.giveFeedback();
+  };
 };
 
 module.exports = Round;
