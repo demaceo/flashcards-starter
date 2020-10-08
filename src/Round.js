a3125f4bb3c65c1bbe5bb53b2b0bd61afbfdbceb
Demaceo Howard <hdemaceo@gmail.com>
2020-10-08 16:22:44 -0600

    Add and create functionality to calculatePercentCorrect method

--- src/Round.js
+++ src/Round.js
@@ -12,16 +12,21 @@ class Round {
   returnCurrentCard() {
     return this.card;
   };
-  
+
   takeTurn(guess) {
     this.turn = new Turn(guess, this.card);
     this.deck.cards.shift();
     this.card = this.deck.cards[0];
-    if(this.turn.evaluateGuess() === false) {
+    if (this.turn.evaluateGuess() === false) {
       this.incorrectGuesses.unshift(this.turn.card.id);
     };
     this.turns += 1;
     return this.turn.giveFeedback();
+  };
+  calculatePercentCorrect() {
+    const correctGuessCount = (this.turns - this.incorrectGuesses.length);
+    const percentCorrect = ((correctGuessCount / this.turns) * 100);
+    return Math.trunc(percentCorrect);
   };
 };
 
